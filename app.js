9dcc4715d4138f04150e5e6eca9199a8fc30452e
syed zohaib <zohaibsyed914@gmail.com>
2024-08-21 07:39:15 +0500

    editing

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ fetch('https://restcountries.com/v3.1/all').then((res) => res.json())
     .then((data) => {
         data.forEach((country) => {
             console.log(country)
-            const countryCard = document.createElement('div')
+            const countryCard = document.createElement('a')
             countryCard.classList.add('country-card')
+            countryCard.href = `./country.html?name=${country.name.common}`
             const cardHTML = `
              <img src="${country.flags.svg}" alt="flag">
                             <div class="card-text">
                                 <h3>${country.name.common}</h3>
-                                <p><b>Population: </b>${country.population}</p>
+                                <p><b>Population: </b>${country.population.toLocaleString()}</p>
                                 <p><b>Region: </b>${country.region}</p>
                                 <p><b>Capital: </b>${country.capital}</p>
-                               <p><b>Language: </b>${country.languages}</p>
+                               <p><b>Language: </b>${Object.values(country.languages)[0]}</p>
                             </div>
             `
             countryCard.innerHTML = cardHTML
